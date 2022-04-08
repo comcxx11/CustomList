d497af5e44294b2c28f8931d23efe713adbc60f1
Seojin Hong <comcxx11@gmail.com>
2022-04-08 19:49:28 +0900

    Initial Commit

---
+++ CustomList/CustomListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomListApp.swift
+//  CustomList
+//
+//  Created by SEOJIN HONG on 2022/04/08.
+//
+
+import SwiftUI
+
+@main
+struct CustomListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
